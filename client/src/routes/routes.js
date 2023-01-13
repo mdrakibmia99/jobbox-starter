9f3d892f20c851f13dcf2b683ab6d607e73b1fb7
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-01-13 20:21:36 +0600

    routes page update

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -1,11 +1,64 @@
 import { createBrowserRouter } from "react-router-dom";
 import Dashboard from "../layout/dashboard/Dashboard";
 import Main from "../layout/main/Main";
-
+import AccountCreator from "../pages/register/AccountCreator";
+import Home from "../pages/home/Home";
+import JobDetails from "../pages/JobDetails";
+import Jobs from "../pages/Jobs";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import PrivateRoute from "../utils/PrivateRoute";
+import AddJob from "../pages/employeeDashboard/AddJob";
+import EmployerDashboard from "../pages/employeeDashboard/EmployerDashboard";
+import CandidateDashboard from "../pages/candidateDashboard/CandidateDashboard";
+import AppliedJobs from "../pages/candidateDashboard/AppliedJobs";
+import JobsList from "../pages/employeeDashboard/JobsList";
+import Candidates from "../pages/Candidates";
 
 const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/jobs",
+        element: <Jobs />,
+      },
+      {
+        path: "/job-details/:id",
+        element: <JobDetails />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/register",
+        element: (
+          <PrivateRoute>
+            <AccountCreator />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/register/:type",
+        element: (
+          <PrivateRoute>
+            <AccountCreator />
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
   
- 
 ]);
 
 export default routes;
