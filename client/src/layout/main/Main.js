b0c3f250123a34f9c4b8c6e39e5f054f29f99a4b
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-01-13 20:39:00 +0600

    create main file in main folder

---
+++ client/src/layout/main/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import Navbar from "./Navbar";
+
+const Main = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <div>
+      
+      </div>
+    </div>
+  );
+};
+
+export default Main;
