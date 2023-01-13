98cdabd10592a8323b0e602f29634a9269d15dbd
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-01-13 20:19:14 +0600

    create routes.js and progress it

---
+++ client/src/routes/routes.js
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import Dashboard from "../layout/dashboard/Dashboard";
+import Main from "../layout/main/Main";
+
+
+const routes = createBrowserRouter([
+  
+ 
+]);
+
+export default routes;
