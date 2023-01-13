3f602ae7c0148e54ca6a9299797325e90135a3a9
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-01-13 19:29:38 +0600

    create store and progress it

---
+++ client/src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import apiSlice from "../features/api/apiSlice";
+import authSlice from "../features/auth/authSlice";
+
+const store = configureStore({
+ 
+});
+
+export default store;
