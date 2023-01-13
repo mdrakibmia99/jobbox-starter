83b98508239c3556d53bc3e5d87b4627d8e3e853
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-01-13 20:17:32 +0600

    crate privete route

---
+++ client/src/utils/PrivateRoute.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate, useLocation } from "react-router-dom";
+import Loading from "../components/reusable/Loading";
+
+const PrivateRoute = ({ children }) => {
+  
+};
+
+export default PrivateRoute;
