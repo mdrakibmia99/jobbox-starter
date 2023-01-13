19cbf68816c60a8c2807631a6dc9ac3141d519d1
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-01-13 20:32:14 +0600

    dashboard progress and add some feature

--- client/src/layout/dashboard/Dashboard.js
+++ client/src/layout/dashboard/Dashboard.js
@@ -5,7 +5,12 @@ import Sidebar from "./Sidebar";
 const Dashboard = () => {
   return (
     <div className="grid grid-cols-12">
-      
+      <Sidebar />
+      <div className=" col-span-10">
+        <div className=" h-full max-w-7xl mx-auto">
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 };
