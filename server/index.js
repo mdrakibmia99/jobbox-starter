6ed033f63c3db1f905e6e9ade88f8d17555be79e
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-01-13 19:22:33 +0600

    connect mongodb

--- server/index.js
+++ server/index.js
@@ -9,7 +9,20 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@todoapp.mfoky.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
+const run = async () => {
+  try {
+    
+  } finally {
+    // await client.close();
+  }
+};
 
 run().catch((err) => console.log(err));
 
