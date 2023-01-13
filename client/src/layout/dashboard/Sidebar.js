772708d6a5a84956faa68c9f5e67006be137905e
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-01-13 20:34:06 +0600

    sidebard added design

--- client/src/layout/dashboard/Sidebar.js
+++ client/src/layout/dashboard/Sidebar.js
@@ -25,7 +25,37 @@ const Sidebar = () => {
 
   return (
     <div className="bg-primary/10 col-span-2 h-screen sticky top-0">
-     
+      <ul className="flex flex-col gap-2 w-full h-full  p-3">
+        <div className="flex justify-between items-center text-primary my-1">
+          <Link to="/" className="flex items-center">
+            <FaChevronLeft />
+            <h1>Back</h1>
+          </Link>
+          <h1 className="text-xl">Dashboard</h1>
+        </div>
+        {user?.role === "employer" &&
+          employerRoutes.map(({ path, name }) => (
+            <li key={name}>
+              <Link
+                className="hover:bg-primary hover:text-white bg-primary/10 transition-all w-full block py-2 px-3 rounded-full"
+                to={path}
+              >
+                {name}
+              </Link>
+            </li>
+          ))}
+        {user?.role === "candidate" &&
+          candidateRoutes.map(({ path, name }) => (
+            <li key={name}>
+              <Link
+                className="hover:bg-primary hover:text-white bg-primary/10 transition-all w-full block py-2 px-3 rounded-full"
+                to={path}
+              >
+                {name}
+              </Link>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 };
