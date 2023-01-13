b8bf9c7b313ccb9f681bc53414614fe027699f3c
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-01-13 20:24:54 +0600

    crate a component folder and reusable file progress

---
+++ client/src/components/reusable/Loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+import loading from "../../assets/loading.gif";
+const Loading = () => {
+  const style = {
+    background: `radial-gradient(circle, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 30%)`,
+  };
+
+  return (
+    
+  );
+};
+
+export default Loading;
