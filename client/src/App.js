61e1728d62750c6190dcf48275923c9dbf7bb6b7
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-01-13 19:28:02 +0600

    index.css and app.css add css and redux

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import auth from "./config/firebase.config";
+import { getUser, toggleLoading } from "./features/auth/authSlice";
+import routes from "./routes/routes";
+import { Toaster } from "react-hot-toast";
+
+function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(getUser(user.email));
+      } else {
+        dispatch(toggleLoading());
+      }
+    });
+  }, [dispatch]);
+
+  return (
+    <>
+      <Toaster />
+      <RouterProvider router={routes} />
+    </>
+  );
+}
+
+export default App;
