4a820647ef512b9b177d7d0fa9c94c0cbb7326f0
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-01-13 20:31:41 +0600

    creat layout folder and create dashboard file in those folder and progreess it

---
+++ client/src/layout/dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+
+const Dashboard = () => {
+  return (
+    <div className="grid grid-cols-12">
+      
+    </div>
+  );
+};
+
+export default Dashboard;
