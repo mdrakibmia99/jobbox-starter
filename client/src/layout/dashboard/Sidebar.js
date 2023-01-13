6d92ca15af4a455e71d0f3e91326b71cf366a800
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-01-13 20:33:30 +0600

    create employeRoutes in array object

--- client/src/layout/dashboard/Sidebar.js
+++ client/src/layout/dashboard/Sidebar.js
@@ -6,7 +6,22 @@ import { useSelector } from "react-redux";
 const Sidebar = () => {
   const { user } = useSelector((state) => state.auth);
 
-
+  const employerRoutes = [
+    {
+      path: "add-job",
+      name: "Add Job",
+    },
+    {
+      path: "job-list",
+      name: "Job List",
+    },
+  ];
+  const candidateRoutes = [
+    {
+      path: "applied-jobs",
+      name: "Applied Jobs",
+    },
+  ];
 
   return (
     <div className="bg-primary/10 col-span-2 h-screen sticky top-0">
